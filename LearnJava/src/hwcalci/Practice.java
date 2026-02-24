58bcb4bccc88636976c03efef9bb15b947f5699d
Divyasha TRADING CO <Divyasha TRADING CO@DESKTOP-399K5H3>
2026-02-24 17:55:34 -0500

    created practice calci

--- LearnJava/src/hwcalci/Practice.java
+++ LearnJava/src/hwcalci/Practice.java
@@ -1,5 +1,14 @@
 package hwcalci;
 
 public class Practice {
+	
+	static byte num1=67;
+	static short num2=55;
+	static int sum= num1+num2;
+	
+	public static void main(String[] args)
+	{
+		System.out.println(sum);
+	}
 
 }
