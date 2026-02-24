3a4280d2ef89f337edcf8c6d891589a5a9c8abd9
Divyasha TRADING CO <Divyasha TRADING CO@DESKTOP-399K5H3>
2026-02-24 18:24:12 -0500

    added difference method

--- LearnJava/src/hwcalci/Practice.java
+++ LearnJava/src/hwcalci/Practice.java
@@ -5,10 +5,17 @@ public class Practice {
 	static byte num1=67;
 	static short num2=55;
 	static int sum= num1+num2;
+
 	
 	public static void main(String[] args)
 	{
 		System.out.println(sum);
+		Practice pr=new Practice();
+		pr.difference(2,7);
 	}
-
+	public void difference(float n1,float n2) {
+		float minus= n1-n2;
+		System.out.println(minus);
+	}
 }
+
